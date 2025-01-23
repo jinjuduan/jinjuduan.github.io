5a4878aaa2a429c5bc176f6e71b461832508a9bd
Juju <jinjuduan@gmail.com>
2025-01-23 20:08:37 +0800

    my github.io v1.0

---
+++ assets/js/tailwind.config.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        clifford: '#da373d',
+      },
+      lineHeight: {
+        '16': '4rem',
+      },
+      rotate: {
+        '30': '30deg',
+      }
+    }
+  }
+}

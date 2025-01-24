48787f2d59621e0e9f8e8ac6497aa2d66807dbe9
Juju <jinjuduan@gmail.com>
2025-01-24 10:50:44 +0800

    box shadow optimize

--- assets/js/tailwind.config.js
+++ assets/js/tailwind.config.js
@@ -1,15 +1,12 @@
 tailwind.config = {
   theme: {
     extend: {
-      colors: {
-        clifford: '#da373d',
-      },
       lineHeight: {
         '16': '4rem',
       },
       rotate: {
         '30': '30deg',
-      }
+      },
     }
   }
 }
